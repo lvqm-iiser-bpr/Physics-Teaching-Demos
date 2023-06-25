a07f315c5d470450c9cc1f11d43cabfde2f0c2b1
AKP2004 <129662980+AKP2004@users.noreply.github.com>
2023-06-25 12:00:25 +0530

    Update WAVE IN SPACE.py

--- Waves/WAVE IN SPACE.py
+++ Waves/WAVE IN SPACE.py
@@ -5,17 +5,17 @@ from matplotlib.widgets import TextBox
 from math import pi as pi
 
 
-# Define the parametrized function to be plotted.
+#Define the parametrized function to be plotted.
 def f(x, amplitude, wavelength, a):
     λ = wavelength
     return amplitude * np.sin(2 * np.pi * x/λ + np.pi * a)
 x = np.linspace(0, 1, 1000)
 
-# Define initial parameters
+#Define initial parameters
 init_amplitude = 8
 init_wavelength = 0.105
 init_a = 0
-# Creating the figure and the line that has to be manipulated.
+#Creating the figure and the line that has to be manipulated.
 fig, ax = plt.subplots()
 line, = ax.plot( x, f(x, init_amplitude, init_wavelength, init_a), color='r')
 ax.set_xlabel('x [m]', color='darkmagenta')
@@ -25,11 +25,11 @@ ax.grid(True)
 plt.yticks(np.arange(-15, 16, 3))
 plt.title('A Wave in space, y = Ae$^{(kx+\phi)}$', fontstyle='oblique', color='navy')
 
-# adjusting the main plot to make room for the sliders.
+#adjusting the main plot to make room for the sliders.
 
 fig.subplots_adjust(bottom=0.40)
 
-# Making horizontal sliders to control the wavelength, the amplitude and the phase.
+#Making horizontal sliders to control the wavelength, the amplitude and the phase.
 axfreq =fig.add_axes([0.26, 0.07, 0.65, 0.03])
 wavelength_slider = Slider(
     ax=axfreq,
@@ -60,13 +60,13 @@ phi_slider = Slider(
     initcolor='r',
     track_color='silver',
 )
-# Remove the slider text to add text box instead.
+#Remove the slider text to add text box instead.
 
 phi_slider.valtext.set_visible(False)
 
 #ax_box = plt.axes([0.2, 0.28, 0.1, 0.07])
 
-# Creating text box for Wavevector, Frequency and also for phase constant to show it in terms of multiples of π.
+#Creating text box for Wavevector, Frequency and also for phase constant to show it in terms of multiples of π.
 
 text = ax.text(-0.1,- 25, f"Wavevector (k) = {round(2*np.pi/wavelength_slider.val,2)} " "m$^{-1}$" " (where k = 2π/λ)")
 text1 = ax.text(1.08, -29, f"{round(phi_slider.val, 2)} π")
@@ -74,7 +74,7 @@ text2 = ax.text(-0.1,- 22, f"Frequency (ν) = {round(1/wavelength_slider.val,2)}
 
 
 
-# Defining the function to be called anytime a slider's value changes
+#Defining the function to be called anytime a slider's value changes
 def update(val):
     global text
     global text1
@@ -96,13 +96,13 @@ def update(val):
 
     fig.canvas.draw_idle()
 
-# registering the update function with each slider
+#registering the update function with each slider
 
 wavelength_slider.on_changed(update)
 amp_slider.on_changed(update)
 phi_slider.on_changed(update)
 
-# Creating a `matplotlib.widgets.Button` to reset the sliders to initial values.
+#Creating a `matplotlib.widgets.Button` to reset the sliders to initial values.
 
 resetax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
 button = Button(resetax, 'Reset', hovercolor='0.975')
